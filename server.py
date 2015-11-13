2d6776beeb6af38f0a14cef261245449d044efdc
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2015-11-13 20:45:44 +0100

    Add react, add simple form.

--- server.py
+++ server.py
@@ -15,9 +15,9 @@ def depts():
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
 
 
