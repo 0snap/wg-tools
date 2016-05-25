b4f6f5173c04469baca819909088680f17240418
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-05-25 15:59:06 +0200

    loc script improved

--- countLOC.sh
+++ countLOC.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-find ./py-backend -name '*.py' -o -name '*.sh' |xargs wc -l|grep total & find ./react-frontend/ -path ./react-frontend/node_modules -prune -o -name '*.js*' -o -name '*.scss' -o -name '*.sh'|xargs wc -l|grep total
+echo '==========================================='
+echo 'LOC py-backend:'
+find ./py-backend -name '*.py' -o -name '*.sh' |xargs wc -l|grep total 
+
+echo ''
+echo 'LOC react-fontend (excluding node_modules):'
+find ./react-frontend/ -path ./react-frontend/node_modules -prune -o -name '*.js*' -print -o -name '*.scss' -print -o -name '*.sh' -print|xargs wc -l|grep total
+echo '==========================================='
