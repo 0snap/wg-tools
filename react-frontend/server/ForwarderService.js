640b4704bdcab69e2b2c9ea88792b2d604883a14
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-06-08 13:51:30 +0200

    fix issue with empty response codes.

--- react-frontend/server/ForwarderService.js
+++ react-frontend/server/ForwarderService.js
@@ -3,17 +3,26 @@ var request = require('superagent');
 
 module.exports = function(endpoint) {
 	return function(req, res, next) {
-		request(req.method, endpoint + req.params.path)
-			.set(req.headers)
-			.send(req.body)
-			.end( function(err, resBackend) {
-				if(err) {
-					console.log("Error forwarding request", endpoint, err.status);
-					res.status(err.status).send(err);
-				}
-				else {
-					res.send(resBackend);
-				}
-			});
+		try {
+			request(req.method, endpoint + req.params.path)
+				.set(req.headers)
+				.send(req.body)
+				.end( function(err, resBackend) {
+					if(err) {
+						console.log('Error forwarding request', endpoint, err);
+						if(err.status > 100 && err.status < 999) {
+							res.status(err.status).send(err);
+						}
+						else res.status(500).send('Undefined status code returned from Backend');
+					}
+					else {
+						res.send(resBackend);
+					}
+				});
+		}
+		catch (ex) {
+			console.log('Exception occurred while forwarding request', req);
+			res.status(500).send('Error');
+		}
 	}
 }
