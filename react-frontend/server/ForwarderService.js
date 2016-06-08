7b5a405876b2174777cc8be3e30d38e0e4a1af2b
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-06-08 18:38:59 +0200

    logging

--- react-frontend/server/ForwarderService.js
+++ react-frontend/server/ForwarderService.js
@@ -3,28 +3,23 @@ var request = require('superagent');
 
 module.exports = function(endpoint) {
 	return function(req, res, next) {
-		try {
-			request(req.method, endpoint + req.params.path)
-				.set(req.headers)
-				.send(req.body)
-				.end( function(err, resBackend) {
-					if(err) {
-						console.log('Error forwarding request', endpoint, err);
-						if(err.status > 100 && err.status < 999) {
-							res.status(err.status).send(err);
-						}
-						else {
-							res.status(500).send('Undefined status code returned from Backend');
-						}
+		console.log('Forwarding request', req.params, req.body, req.headers);
+		request(req.method, endpoint + req.params.path)
+			.set(req.headers)
+			.send(req.body)
+			.end( function(err, resBackend) {
+				if(err) {
+					console.log('Error forwarding request', endpoint, err);
+					if(err.status > 100 && err.status < 999) {
+						res.status(err.status).send(err);
 					}
 					else {
-						res.send(resBackend);
+						res.status(500).send('Undefined status code returned from Backend');
 					}
-				});
-		}
-		catch (ex) {
-			console.log('Exception occurred while forwarding request', req);
-			res.status(500).send('Error');
-		}
+				}
+				else {
+					res.send(resBackend);
+				}
+			});
 	}
 }
