9910434bcabe28bddf71f601aafba5dd80be9c2a
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-06-08 13:57:29 +0200

    brackets

--- react-frontend/server/ForwarderService.js
+++ react-frontend/server/ForwarderService.js
@@ -13,7 +13,9 @@ module.exports = function(endpoint) {
 						if(err.status > 100 && err.status < 999) {
 							res.status(err.status).send(err);
 						}
-						else res.status(500).send('Undefined status code returned from Backend');
+						else {
+							res.status(500).send('Undefined status code returned from Backend');
+						}
 					}
 					else {
 						res.send(resBackend);
